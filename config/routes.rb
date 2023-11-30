23d43fce235cb7e651d1febf159a9b86ca3d9683
kei-prog <kei08prog@gmail.com>
2023-12-01 08:34:58 +0900

    routes設定の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'users', controllers: {
+        registrations: 'api/v1/users/registrations'
+      }
+    end
+  end
+
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
