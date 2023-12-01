7dc06641c7ac7c0fa37e31d4001d1d7a8b257884
kei-prog <kei08prog@gmail.com>
2023-12-01 13:25:04 +0900

    ユーザシードの追加

---
+++ db/seeds/users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+User.create!(
+  email: 'user@example.com',
+  name: 'Example User',
+  password: 'password',
+  password_confirmation: 'password',
+  birthday: '1990-01-01',
+  introduction: 'Hello, I am an example user.',
+  location: 'Tokyo',
+  website: 'https://example.com'
+)
