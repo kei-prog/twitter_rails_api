dffce929ac2d23b71d30f8bb3db467ddc3263905
kei-prog <kei08prog@gmail.com>
2023-12-26 02:38:05 +0900

    ログイン機能のroutes修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/users/registrations',
-        confirmations: 'api/v1/users/confirmations'
+        confirmations: 'api/v1/users/confirmations',
+        sessions: 'api/v1/users/sessions'
       }
     end
   end
