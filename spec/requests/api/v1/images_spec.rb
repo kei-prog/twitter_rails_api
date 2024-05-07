9ad5e7395679e11b526527edc0b15a5229cf3611
kei-prog <kei08prog@gmail.com>
2024-05-07 15:37:50 +0900

    rubocop実行

--- spec/requests/api/v1/images_spec.rb
+++ spec/requests/api/v1/images_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Images", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/images/create"
+RSpec.describe 'Api::V1::Images' do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/api/v1/images/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
