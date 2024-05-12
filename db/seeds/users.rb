93bfb35efcae25c6964c57dd4ca5ff971e112368
kei-prog <kei08prog@gmail.com>
2024-05-12 20:46:37 +0900

    ユーザシードを認証済み状態に修正

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -5,6 +5,7 @@ User.create!(
   name: 'Example User',
   password: 'password',
   password_confirmation: 'password',
+  confirmed_at: Time.zone.now,
   birthday: '1990-01-01',
   introduction: 'Hello, I am an example user.',
   location: 'Tokyo',
