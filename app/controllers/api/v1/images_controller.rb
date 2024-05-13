0e623e27b4d806bfbb21a9e603c248273dc8896e
kei-prog <kei08prog@gmail.com>
2024-05-13 16:31:33 +0900

    イメージコントローラー実装

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -3,7 +3,24 @@
 module Api
   module V1
     class ImagesController < ApplicationController
-      def create; end
+      before_action :authenticate_api_v1_user!
+
+      def create
+        tweet = current_api_v1_user.tweets.find(image_params[:tweet_id])
+        tweet.images.attach(image_params[:images])
+
+        if tweet.images.attached? && tweet.valid?
+          head :created
+        else
+          render json: { errors: tweet.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def image_params
+        params.permit(:tweet_id, images: [])
+      end
     end
   end
 end
