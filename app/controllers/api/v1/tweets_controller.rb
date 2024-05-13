7b20b277c60bbd4ff977b5fbe88c944c92621554
kei-prog <kei08prog@gmail.com>
2024-05-13 16:31:11 +0900

    ツイートコントローラー実装

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,7 +3,22 @@
 module Api
   module V1
     class TweetsController < ApplicationController
-      def create; end
+      before_action :authenticate_api_v1_user!
+
+      def create
+        @tweet = current_api_v1_user.tweets.build(tweet_params)
+        if @tweet.save
+          render json: @tweet, status: :created
+        else
+          render json: @tweet.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def tweet_params
+        params.require(:tweet).permit(:body)
+      end
     end
   end
 end
