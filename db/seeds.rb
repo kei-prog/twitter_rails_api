ac58efc9e0c2d1c41bffba04786f278d5874b485
kei-prog <kei08prog@gmail.com>
2024-05-28 21:26:04 +0900

    シード読み込み対象をseedsフォルダ配下に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require './db/seeds//users'
+Dir[Rails.root.join('db/seeds/**/*.rb')].each do |seed|
+  load seed
+end
