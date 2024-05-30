ca1fd8747f10f5e406056004db4f05fb36ffdbf8
kei-prog <kei08prog@gmail.com>
2024-05-30 17:35:22 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
         sessions: 'api/v1/users/sessions'
       }
 
-      resources :tweets, only: %i[index create] do
+      resources :tweets, only: %i[index create show] do
         resources :images, only: %i[create]
       end
     end
