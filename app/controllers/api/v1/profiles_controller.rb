63d2ca64c22333dfdf00dbbd3d4e7c978f3b0285
kei-prog <kei08prog@gmail.com>
2024-06-05 18:11:42 +0900

    プロフィールコントローラー実装

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,7 +3,29 @@
 module Api
   module V1
     class ProfilesController < ApplicationController
-      def update; end
+      before_action :authenticate_api_v1_user!, only: %i[update]
+
+      def update
+        if current_api_v1_user.update(profile_params)
+          attach_files if params[:header].present? || params[:avatar].present?
+          render json: current_api_v1_user, status: :ok
+        else
+          render json: { errors: current_api_v1_user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      PROFILE_PARAMS = %i[name introduction location website].freeze
+
+      def profile_params
+        params.permit(PROFILE_PARAMS)
+      end
+
+      def attach_files
+        current_api_v1_user.header.attach(params[:header]) if params[:header].present?
+        current_api_v1_user.avatar.attach(params[:avatar]) if params[:avatar].present?
+      end
     end
   end
 end
