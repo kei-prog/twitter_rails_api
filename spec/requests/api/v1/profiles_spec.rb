6e283b6d1f8f5cf3cdca6cb2e94c41c76e4abdce
kei-prog <kei08prog@gmail.com>
2024-06-05 19:18:37 +0900

    プロフィールコントローラーのリクエストスペック実装

--- spec/requests/api/v1/profiles_spec.rb
+++ spec/requests/api/v1/profiles_spec.rb
@@ -3,10 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Profiles' do
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/api/v1/profiles/update'
-      expect(response).to have_http_status(:success)
+  describe 'PATCH /update' do
+    let(:user) { create(:user) }
+    let(:valid_attributes) do
+      { name: 'Update Name',
+        introduction: 'Update Introduction',
+        location: 'Tokyo',
+        website: 'www.example.com' }
+    end
+
+    before do
+      sign_in user
+    end
+
+    context 'when valid parameters' do
+      it 'updates the user profile' do
+        patch api_v1_profile_path, params: valid_attributes
+        user.reload
+        expect(user).to have_attributes(valid_attributes)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'returns 422 for invalid data' do
+        patch api_v1_profile_path, params: { name: '' }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
