c1c78c0d9f70129795f198cbe99d46cb72d12a5b
kei-prog <kei08prog@gmail.com>
2024-06-12 01:41:51 +0900

    コメントコントローラーの実装

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,6 +3,22 @@
 module Api
   module V1
     class CommentsController < ApplicationController
+      before_action :authenticate_api_v1_user!
+
+      def create
+        comment = current_api_v1_user.comments.build(comment_params)
+        if comment.save
+          render json: comment, status: :created
+        else
+          render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:body).merge(tweet_id: params[:tweet_id])
+      end
     end
   end
 end
