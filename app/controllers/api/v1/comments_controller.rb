36740a02074b7314d58f005de46deb757f385108
kei-prog <kei08prog@gmail.com>
2024-06-12 18:14:16 +0900

    rubocop実行

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,8 @@
-class Api::V1::CommentsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CommentsController < ApplicationController
+    end
+  end
 end
