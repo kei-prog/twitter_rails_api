d65cab6da1e4963ec001a60ce911407f332016bb
kei-prog <kei08prog@gmail.com>
2024-06-15 03:17:34 +0900

    コメント一覧取得のindexアクション追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,6 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validate_body_byte_size :body
+
+  scope :recent, ->(offset, limit) { order(created_at: :desc).offset(offset).limit(limit) }
 end
