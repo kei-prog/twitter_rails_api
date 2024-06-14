6140a5c8ff641954da3dccd79ef5f23afa8a3681
kei-prog <kei08prog@gmail.com>
2024-06-15 04:03:24 +0900

    indexアクションのリクエストスペックの追加

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -8,6 +8,41 @@ RSpec.describe 'Api::V1::Comments' do
   let(:valid_attributes) { { body: 'ValidBody' } }
   let(:invalid_attributes) { { body: 'a' * 281 } }
 
+  describe 'GET /index' do
+    before do
+      sign_in user
+    end
+
+    context 'when valid parameters' do
+      it 'returns a list of comments' do
+        create_list(:comment, 10, tweet:, user:)
+        get api_v1_tweet_comments_path(tweet)
+        json = response.parsed_body
+        expect(json).not_to be_empty
+      end
+
+      it 'returns a 200 status code' do
+        get api_v1_tweet_comments_path(tweet)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when invalid parameters' do
+      before do
+        get api_v1_tweet_comments_path(tweet, offset: -1)
+      end
+
+      it 'failed to get comments' do
+        json = response.parsed_body
+        expect(json['errors']).to eq([I18n.t('query_parameters.comments.invalid_query_parameters')])
+      end
+
+      it 'returns a 400 status code' do
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
+
   describe 'POST /create' do
     before do
       sign_in user
