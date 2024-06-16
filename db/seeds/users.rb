60dbce551d652fa18e781df598cc5a78ad59e5e7
kei-prog <kei08prog@gmail.com>
2024-06-17 00:54:21 +0900

    コメントシード追加

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+users = []
+
 5.times do |i|
   user = User.create!(
     email: "user#{i + 1}@example.com",
@@ -13,10 +15,26 @@
     website: "https://example#{i + 1}.com"
   )
 
+  tweets = []
+
   10.times do |j|
-    Tweet.create!(
+    tweet = Tweet.create!(
       user:,
       body: "User#{i + 1} Seed Tweet #{j + 1}"
     )
+    tweets << tweet
+  end
+
+  users << { user:, last_tweet: tweets.last }
+end
+users.each do |user|
+  users.each do |target_data|
+    5.times do |k|
+      Comment.create!(
+        user: user[:user],
+        tweet: target_data[:last_tweet],
+        body: "Comment #{k + 1}"
+      )
+    end
   end
 end
