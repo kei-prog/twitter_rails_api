d580bdaaf709db9a428a98dc5439916304e6aa55
kei-prog <kei08prog@gmail.com>
2024-06-17 19:46:44 +0900

    コメント削除のリクエストスペック追加

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -74,4 +74,38 @@ RSpec.describe 'Api::V1::Comments' do
       end
     end
   end
+
+  describe 'DELETE /destroy' do
+    let!(:comment) { create(:comment, user:, tweet:) }
+
+    before do
+      sign_in user
+    end
+
+    context 'when valid parameters' do
+      it 'deletes the comment' do
+        expect do
+          delete api_v1_comment_path(comment)
+        end.to change(Comment, :count).by(-1)
+      end
+
+      it 'returns a 204 status code' do
+        delete api_v1_comment_path(comment)
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'returns a 404 status code' do
+        delete api_v1_comment_path(id: 0)
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns an error message' do
+        delete api_v1_comment_path(id: 0)
+        json = response.parsed_body
+        expect(json['errors']).to eq([I18n.t('activerecord.errors.models.comment.not_found')])
+      end
+    end
+  end
 end
