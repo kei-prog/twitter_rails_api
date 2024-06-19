6250ed4b9581998df2c7837f4d3ab24719fa9ebf
kei-prog <kei08prog@gmail.com>
2024-06-20 02:57:58 +0900

    rubocop実行

--- app/controllers/api/v1/retweets_controller.rb
+++ app/controllers/api/v1/retweets_controller.rb
@@ -1,2 +1,8 @@
-class Api::V1::RetweetsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class RetweetsController < ApplicationController
+    end
+  end
 end
