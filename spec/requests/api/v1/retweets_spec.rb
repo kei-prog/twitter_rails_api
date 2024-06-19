27721e6da37906ea0172145018a93e3e6674e57b
kei-prog <kei08prog@gmail.com>
2024-06-20 03:01:34 +0900

    リクエストスペックの追加

--- spec/requests/api/v1/retweets_spec.rb
+++ spec/requests/api/v1/retweets_spec.rb
@@ -3,7 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Retweets' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'POST /toggle_retweet' do
+    let(:user) { create(:user) }
+    let(:tweet) { create(:tweet, user:) }
+
+    before do
+      sign_in user
+    end
+
+    context 'when createing a retweet' do
+      it 'creates a Retweet' do
+        expect do
+          post api_v1_tweet_toggle_retweet_path(tweet)
+        end.to change(Retweet, :count).by(1)
+      end
+
+      it 'returns a 201 status code' do
+        post api_v1_tweet_toggle_retweet_path(tweet)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'when deleting a retweet' do
+      before do
+        post api_v1_tweet_toggle_retweet_path(tweet)
+      end
+
+      it 'destroys the Retweet' do
+        expect do
+          post api_v1_tweet_toggle_retweet_path(tweet)
+        end.to change(Retweet, :count).by(-1)
+      end
+
+      it 'returns a no_content status code' do
+        post api_v1_tweet_toggle_retweet_path(tweet)
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'raises an ActiveRecord::RecordInvalid error' do
+        expect do
+          post api_v1_tweet_toggle_retweet_path(tweet_id: 0)
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
