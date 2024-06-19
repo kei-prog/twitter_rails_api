5988ad5be7879e5798ac39e1c6de88a719cf1d32
kei-prog <kei08prog@gmail.com>
2024-06-20 02:59:43 +0900

    リツイートコントローラーにtoggled_retweetアクションを追加

--- app/controllers/api/v1/retweets_controller.rb
+++ app/controllers/api/v1/retweets_controller.rb
@@ -3,6 +3,18 @@
 module Api
   module V1
     class RetweetsController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[toggle_retweet]
+
+      def toggle_retweet
+        retweet = current_api_v1_user.retweets.find_by(tweet_id: params[:tweet_id])
+        if retweet
+          retweet.destroy!
+          head :no_content
+        else
+          current_api_v1_user.retweets.create!(tweet_id: params[:tweet_id])
+          head :created
+        end
+      end
     end
   end
 end
