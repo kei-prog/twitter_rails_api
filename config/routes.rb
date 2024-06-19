d2b776e4243d530c02636f1cf880f7e9bd691cef
kei-prog <kei08prog@gmail.com>
2024-06-19 23:56:13 +0900

    リツイートのtoggle_retweetルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       resources :tweets, only: %i[index create show destroy] do
         resources :images, only: %i[create]
         resources :comments, only: %i[index create]
+        post 'toggle_retweet', to: 'retweets#toggle_retweet'
       end
 
       resources :comments, only: %i[destroy]
