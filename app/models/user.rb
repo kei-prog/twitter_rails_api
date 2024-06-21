df18fed0baff3b2c5f26260259683cbd93d09cab
kei-prog <kei08prog@gmail.com>
2024-06-22 01:58:12 +0900

    モデルの関連付けを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :retweets, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy,
+                            inverse_of: :follower
+  has_many :following, through: :active_follows, source: :followed
+  has_many :passive_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy,
+                             inverse_of: :followed
+  has_many :followers, through: :passive_follows, source: :follower
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
