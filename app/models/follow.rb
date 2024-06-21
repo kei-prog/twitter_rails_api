4c4a299839b408a5294cdb9fed0b7f687db3d5ad
kei-prog <kei08prog@gmail.com>
2024-06-22 01:29:51 +0900

    rubocop実行

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Follow < ApplicationRecord
   belongs_to :follower
   belongs_to :followed
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Follow, type: :model do
+RSpec.describe Follow do
   pending "add some examples to (or delete) #{__FILE__}"
 end
