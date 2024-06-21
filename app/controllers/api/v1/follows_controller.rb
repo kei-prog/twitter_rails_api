a045689da03cc79b7f85a1fa09d283590beeb375
kei-prog <kei08prog@gmail.com>
2024-06-22 04:01:44 +0900

    rubocop実行

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -1,2 +1,8 @@
-class Api::V1::FollowsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class FollowsController < ApplicationController
+    end
+  end
 end
