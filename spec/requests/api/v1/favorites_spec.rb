3e6ffa7f3dbc10049bce8ba8fa2b00e50af6eda7
kei-prog <kei08prog@gmail.com>
2024-06-21 19:41:36 +0900

    リクエストスペックの追加

--- spec/requests/api/v1/favorites_spec.rb
+++ spec/requests/api/v1/favorites_spec.rb
@@ -3,7 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Favorites' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'POST /toggle_favorite' do
+    let(:user) { create(:user) }
+    let(:tweet) { create(:tweet, user:) }
+
+    before do
+      sign_in user
+    end
+
+    context 'when createing a favorite' do
+      it 'creates a favorite' do
+        expect do
+          post api_v1_tweet_toggle_favorite_path(tweet)
+        end.to change(Favorite, :count).by(1)
+      end
+
+      it 'returns a 201 status code' do
+        post api_v1_tweet_toggle_favorite_path(tweet)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'when deleting a favorite' do
+      before do
+        post api_v1_tweet_toggle_favorite_path(tweet)
+      end
+
+      it 'destroys the favorite' do
+        expect do
+          post api_v1_tweet_toggle_favorite_path(tweet)
+        end.to change(Favorite, :count).by(-1)
+      end
+
+      it 'returns a no_content status code' do
+        post api_v1_tweet_toggle_favorite_path(tweet)
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'raises an ActiveRecord::RecordInvalid error' do
+        expect do
+          post api_v1_tweet_toggle_favorite_path(tweet_id: 0)
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
