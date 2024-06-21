5579b973493d4f80ca171ed60f7858fd041144ab
kei-prog <kei08prog@gmail.com>
2024-06-21 19:03:44 +0900

    toggle_favoriteアクションを追加

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,6 +3,18 @@
 module Api
   module V1
     class FavoritesController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[toggle_favorite]
+
+      def toggle_favorite
+        favorite = current_api_v1_user.favorites.find_by(tweet_id: params[:tweet_id])
+        if favorite
+          favorite.destroy!
+          head :no_content
+        else
+          current_api_v1_user.favorites.create!(tweet_id: params[:tweet_id])
+          head :created
+        end
+      end
     end
   end
 end
