5599e9bd9b598f789d7afd44419c61e34ff739c2
kei-prog <kei08prog@gmail.com>
2024-06-22 22:21:29 +0900

    リクエストスペックの追加

--- spec/requests/api/v1/follows_spec.rb
+++ spec/requests/api/v1/follows_spec.rb
@@ -3,7 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Follows' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'POST /create' do
+    let(:user) { create(:user) }
+    let(:followed_user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
+    context 'when creating a follow' do
+      it 'creates a Follow' do
+        expect do
+          post api_v1_user_follows_path(user, user_id: followed_user.id)
+        end.to change(Follow, :count).by(1)
+      end
+
+      it 'returns a 201 status code' do
+        post api_v1_user_follows_path(user, user_id: followed_user.id)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'raises an ActiveRecord::RecordInvalid error' do
+        expect do
+          post api_v1_user_follows_path(user, user_id: 0)
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
