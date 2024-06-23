2d338b66517029a19a281b59bed1679ac21aea13
kei-prog <kei08prog@gmail.com>
2024-06-24 02:29:34 +0900

    migrationファイル修正

--- db/migrate/20240709084815_create_notifications.rb
+++ db/migrate/20240709084815_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :send_user, null: false, foreign_key: true
+      t.references :send_user, null: false, foreign_key: { to_table: :users }
       t.integer :notification_type
       t.references :target, polymorphic: true, null: false
 
