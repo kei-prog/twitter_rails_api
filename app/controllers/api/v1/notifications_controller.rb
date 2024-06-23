2a6af03a16aaed05fe4060090b006b0419e50e48
kei-prog <kei08prog@gmail.com>
2024-06-24 03:10:40 +0900

    rubocop実行

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,2 +1,8 @@
-class Api::V1::NotificationsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class NotificationsController < ApplicationController
+    end
+  end
 end
