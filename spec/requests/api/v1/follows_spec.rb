d0ce60ec9940c840d9c99f5e8242a8beeb4e7b75
kei-prog <kei08prog@gmail.com>
2024-06-23 19:10:05 +0900

    フォロー解除のリクエストスペックを追加

--- spec/requests/api/v1/follows_spec.rb
+++ spec/requests/api/v1/follows_spec.rb
@@ -32,4 +32,35 @@ RSpec.describe 'Api::V1::Follows' do
       end
     end
   end
+
+  describe 'DELETE /destroy' do
+    let(:user) { create(:user) }
+    let(:followed_user) { create(:user) }
+
+    before do
+      sign_in user
+      post api_v1_user_follows_path(user_id: followed_user.id)
+    end
+
+    context 'when deleting a follow' do
+      it 'destroys a Follow' do
+        expect do
+          delete api_v1_user_follows_path(user_id: followed_user.id)
+        end.to change(Follow, :count).by(-1)
+      end
+
+      it 'returns a 204 status code' do
+        delete api_v1_user_follows_path(user_id: followed_user.id)
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when invalid parameters' do
+      it 'raises an ActiveRecord::RecordNotFound error' do
+        expect do
+          delete api_v1_user_follows_path(user_id: 0)
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
