fda651ea547b55bc0b7c338e02989a7611a58076
kei-prog <kei08prog@gmail.com>
2024-06-26 05:29:00 +0900

    通知コントローラーを実装

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -3,6 +3,15 @@
 module Api
   module V1
     class NotificationsController < ApplicationController
+      include PaginationParamsValidator
+
+      before_action :authenticate_api_v1_user!, only: %i[index]
+      before_action :validate_query_params, only: %i[index]
+
+      def index
+        notifications = current_api_v1_user.notifications.recent(@offset, @limit)
+        render json: notifications, status: :ok
+      end
     end
   end
 end
