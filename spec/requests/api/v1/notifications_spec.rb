3f5f85cd066e0ba92b831bc5e9505908b7085cb1
kei-prog <kei08prog@gmail.com>
2024-06-28 08:08:39 +0900

    通知リクエストスペックの追加

--- spec/requests/api/v1/notifications_spec.rb
+++ spec/requests/api/v1/notifications_spec.rb
@@ -3,7 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Notifications' do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      create_list(:notification, 10, user:)
+    end
+
+    context 'when valid parameters' do
+      it 'returns a list of notifications' do
+        get api_v1_notifications_path
+        json = response.parsed_body
+        expect(json.length).to eq(10)
+      end
+
+      it 'returns a 200 status code' do
+        get api_v1_notifications_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when invalid parameters' do
+      before do
+        get api_v1_notifications_path(offset: -1)
+      end
+
+      it 'failed to get notifications' do
+        json = response.parsed_body
+        expect(json['errors']).to eq([I18n.t('query_parameters.tweets.invalid_query_parameters')])
+      end
+
+      it 'returns a 400 status code' do
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
   end
 end
