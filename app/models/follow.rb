967dea37b5b1f6571eddc406598f79614fa89c3b
kei-prog <kei08prog@gmail.com>
2024-07-03 17:41:46 +0900

    フォロー時に通知データを作成するように修正

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class Follow < ApplicationRecord
+  has_many :notifications, as: :target, dependent: :destroy
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
 
   validates :follower_id, uniqueness: { scope: :followed_id }
+
+  def create_notification(current_user)
+    existing_notification = followed.notifications.find_by(notification_type: :follow,
+                                                           send_user: current_user)
+
+    return if existing_notification
+
+    followed.notifications.create!(notification_type: :follow,
+                                   send_user: current_user,
+                                   target: self)
+  end
 end
