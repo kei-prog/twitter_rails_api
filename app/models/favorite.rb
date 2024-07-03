fbd35ab6523ab985b9f958c9555388121d8d0234
kei-prog <kei08prog@gmail.com>
2024-07-03 16:39:03 +0900

    いいね時に通知データを作成するように修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Favorite < ApplicationRecord
+  has_many :notifications, as: :target, dependent: :destroy
   belongs_to :user
   belongs_to :tweet
+
+  def create_notification(current_user)
+    existing_notification = tweet.user.notifications.find_by(notification_type: :like,
+                                                             send_user: current_user)
+
+    return if existing_notification
+
+    tweet.user.notifications.create!(notification_type: :like,
+                                     send_user: current_user,
+                                     target: self)
+  end
 end
