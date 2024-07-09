b123787a1258b87210093bfb679f4919be27f615
kei-prog <kei08prog@gmail.com>
2024-07-09 17:48:29 +0900

    rails generate model Notification user:references send_user:references notification_type:integer target:references{polymorphic}

---
+++ db/migrate/20240709084815_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :send_user, null: false, foreign_key: true
+      t.integer :notification_type
+      t.references :target, polymorphic: true, null: false
+
+      t.timestamps
+    end
+  end
+end
