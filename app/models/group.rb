960fa715efebab36b53a761c6c1b2b135470e7aa
kei-prog <kei08prog@gmail.com>
2024-10-12 18:07:55 +0900

    group modelを実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
-  belongs_to :sender
-  belongs_to :recipient
+  include Sortable
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
+  has_many :messages, dependent: :destroy
+
+  validate :unique_sender_and_recipient
+
+  scope :for_user, lambda { |user_id, offset, limit|
+    where(sender_id: user_id).or(where(recipient_id: user_id)).recent(offset, limit)
+  }
+
+  def as_user_display_json(current_user_id)
+    latest_message = Message.latest_from_other_user(id, current_user_id).first
+
+    user_key = current_user_id == sender_id ? :recipient : :sender
+
+    as_json.merge(
+      sender: send(user_key).as_json(only: %i[id name], methods: :avatar_url),
+      latest_message: latest_message&.slice(:id, :content)
+    )
+  end
+
+  def self.exists?(sender_id:, recipient_id:)
+    where(sender_id:, recipient_id:).or(
+      where(sender_id: recipient_id, recipient_id: sender_id)
+    ).exists?
+  end
+
+  private
+
+  def unique_sender_and_recipient
+    return unless sender_id == recipient_id
+
+    errors.add(:base, I18n.t('activerecord.errors.models.group.same_user'))
+  end
 end
