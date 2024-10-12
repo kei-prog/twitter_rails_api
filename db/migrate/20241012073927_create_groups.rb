b23e4d6c9f87064e82426649d8da548ce8d3ee92
kei-prog <kei08prog@gmail.com>
2024-10-12 16:51:41 +0900

    外部キー参照先をusersに設定

--- db/migrate/20241012073927_create_groups.rb
+++ db/migrate/20241012073927_create_groups.rb
@@ -1,8 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.references :sender, null: false, foreign_key: true
-      t.references :recipient, null: false, foreign_key: true
+      t.references :sender, null: false, foreign_key: { to_table: :users }
+      t.references :recipient, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
