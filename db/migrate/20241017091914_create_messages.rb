f23c244c335d6d308744b161c6465a459890bc98
kei-prog <kei08prog@gmail.com>
2024-10-17 18:34:56 +0900

    migrateファイル修正

--- db/migrate/20241017091914_create_messages.rb
+++ db/migrate/20241017091914_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.references :group, null: false, foreign_key: true
-      t.references :sender, null: false, foreign_key: true
-      t.text :content
+      t.references :sender, null: false, foreign_key: { to_table: :users }
+      t.text :content, null: false
 
       t.timestamps
     end
