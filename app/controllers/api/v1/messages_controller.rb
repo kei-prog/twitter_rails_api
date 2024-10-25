6b91287e1bc3d9a39e2ee9922379d69ae0d4d449
kei-prog <kei08prog@gmail.com>
2024-10-26 06:31:02 +0900

    messages controllerにindex,createアクション追加

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,6 +3,24 @@
 module Api
   module V1
     class MessagesController < ApplicationController
+      include PaginationParamsValidator
+      before_action :authenticate_api_v1_user!
+      before_action :validate_query_params, only: %i[index]
+
+      def index
+        messages = Message.for_group(params[:group_id], @offset, @limit)
+        render json: messages.as_json(include: { sender: { only: %i[id] } }), status: :ok
+      end
+
+      def create
+        message = Message.new(sender_id: current_api_v1_user.id, group_id: params[:group_id],
+                              content: params[:content])
+        if message.save
+          render json: message.as_json(include: { sender: { only: %i[id] } }), status: :created
+        else
+          head :unprocessable_entity
+        end
+      end
     end
   end
 end
