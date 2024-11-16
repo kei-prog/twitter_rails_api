1adcd92958e1f53b9b2997dc29ba1cc20d89ac58
kei-prog <kei08prog@gmail.com>
2024-11-16 09:24:58 +0900

    group model specの追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    context 'when checking the uniqueness of the sender_id and recipient_id' do
+      let(:sender) { create(:user) }
+      let(:recipient) { create(:user) }
+
+      it 'is invalid with the same sender_id and recipient_id' do
+        group = build(:group, sender:, recipient: sender)
+        expect(group).to be_invalid
+        expect(group.errors[:base]).to include(I18n.t('activerecord.errors.models.group.same_user'))
+      end
+    end
+  end
 end
