69fa8e28641f1a371ead369594ddd055a15a415e
kei-prog <kei08prog@gmail.com>
2025-01-16 10:50:31 +0900

    GroupsController index,createアクションの実装

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -3,6 +3,34 @@
 module Api
   module V1
     class GroupsController < ApplicationController
+      include PaginationParamsValidator
+
+      before_action :authenticate_api_v1_user!
+      before_action :validate_query_params, only: %i[index]
+
+      def index
+        groups = Group.for_user(current_api_v1_user.id, @offset, @limit)
+
+        groups_json = groups.map do |group|
+          group.as_user_display_json(current_api_v1_user.id)
+        end
+
+        render json: groups_json, status: :ok
+      end
+
+      def create
+        if Group.exists?(sender_id: current_api_v1_user.id, recipient_id: params[:recipient_id])
+          head :ok
+          return
+        end
+
+        group = Group.new(sender_id: current_api_v1_user.id, recipient_id: params[:recipient_id])
+        if group.save
+          head :created
+        else
+          render json: { errors: group.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
     end
   end
 end
