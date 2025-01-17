179c8ce5e2bdec579ca165bfbda98b54e163f64f
kei-prog <kei08prog@gmail.com>
2025-01-17 18:07:31 +0900

    rubocop実行

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,7 +1,11 @@
-class Api::V1::BookmarksController < ApplicationController
-  def index
-  end
+# frozen_string_literal: true
 
-  def create
+module Api
+  module V1
+    class BookmarksController < ApplicationController
+      def index; end
+
+      def create; end
+    end
   end
 end
