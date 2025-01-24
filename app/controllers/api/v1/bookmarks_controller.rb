f659807b62aa148bf2a0d45617d4f9539d308ef4
kei-prog <kei08prog@gmail.com>
2025-01-24 16:22:38 +0900

    Controller修正 Tweets,Bookmarks
    BookmarksController: index,createアクションの実装
    TweetsController: Bookmarkのステータスを返却するように修正

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -3,9 +3,29 @@
 module Api
   module V1
     class BookmarksController < ApplicationController
-      def index; end
+      include PaginationParamsValidator
 
-      def create; end
+      before_action :authenticate_api_v1_user!
+      before_action :validate_query_params, only: %i[index]
+
+      def index
+        tweets = current_api_v1_user.bookmarked_tweets.recent(@offset, @limit)
+
+        render json: tweets.as_json(include: { user: { methods: :avatar_url, only: %i[id name] } },
+                                    methods: %i[retweet_count favorite_count],
+                                    current_user: current_api_v1_user), status: :ok
+      end
+
+      def toggle_bookmark
+        bookmark = current_api_v1_user.bookmarks.find_by(tweet_id: params[:tweet_id])
+        if bookmark
+          bookmark.destroy!
+          head :no_content
+        else
+          current_api_v1_user.bookmarks.create!(tweet_id: params[:tweet_id])
+          head :created
+        end
+      end
     end
   end
 end
